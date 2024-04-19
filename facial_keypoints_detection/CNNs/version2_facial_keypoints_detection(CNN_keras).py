ce163e08c457106649f92ed75170730cefdd05af
GeekNekoS <anastasiyanesterenko@icloud.com>
2024-04-20 02:23:28 +0500

    Advanced functional net (changes needed)

--- facial_keypoints_detection/CNNs/version2_facial_keypoints_detection(CNN_keras).py
+++ facial_keypoints_detection/CNNs/version2_facial_keypoints_detection(CNN_keras).py
@@ -2,7 +2,12 @@ import keras
 import pandas as pd
 from facial_keypoints_detection.data_preprocessing.images_preprocessing import (preprocessing_X, preprocessing_y)
 from keras.optimizers import Adam
+import tensorflow as tf
 
+
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+if gpu_devices:
+   tf.config.experimental.set_memory_growth(gpu_devices[0], True)
 
 train_path = '../data_for_learning/training.csv'
 train = pd.read_csv(train_path)
@@ -12,21 +17,24 @@ y_train = preprocessing_y(train)
 
 
 model = keras.Sequential([
-    keras.layers.Input((1, 96, 96), batch_size=32, dtype='float32'),
-    keras.layers.Conv2D(32, (3, 3), strides=1, activation='relu', input_shape=(1, 96, 96), data_format='channels_first'),
+    keras.layers.Input((1, 96, 96), batch_size=16, dtype='float32'),
+    keras.layers.Conv2D(32, (3, 3), activation='relu'),
     keras.layers.MaxPooling2D((2, 2), strides=1),
-    keras.layers.Conv2D(64, (2, 2), strides=1, activation='relu', data_format='channels_first'),
+    keras.layers.Conv2D(64, (3, 3), strides=1, activation='relu'),
     keras.layers.MaxPooling2D((2, 2), strides=1),
-    keras.layers.Conv2D(128, (2, 2), strides=1, activation='relu', data_format='channels_first'),
+    keras.layers.Conv2D(128, (3, 3), strides=1, activation='relu'),
     keras.layers.MaxPooling2D((2, 2), strides=1),
     keras.layers.Flatten(),
-    keras.layers.Dense(500, activation='relu'),
-    keras.layers.Dense(500, activation='relu'),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dropout(0.3),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dropout(0.3),
+    keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(30),
 ])
 
 
 optimizer = Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='mae', metrics=['accuracy'])
-history = model.fit(X_train.reshape(-1, 1, 96, 96), y_train, epochs=3, batch_size=32, validation_split=0.2)
+history = model.fit(X_train.reshape(-1, 1, 96, 96), y_train, epochs=100, batch_size=32, validation_split=0.2)
 model.save('facial_keypoints_detection(CNN, 3 epochs).keras')
