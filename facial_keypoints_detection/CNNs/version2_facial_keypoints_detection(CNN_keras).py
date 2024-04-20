7cf961a1def41ac56a0a5870ae639ad7aa95fafd
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-04-20 13:58:38 +0500

    Changed the process of dividing the dataset into a training set and a validation set

--- facial_keypoints_detection/CNNs/version2_facial_keypoints_detection(CNN_keras).py
+++ facial_keypoints_detection/CNNs/version2_facial_keypoints_detection(CNN_keras).py
@@ -1,24 +1,19 @@
 import keras
 import pandas as pd
-from facial_keypoints_detection.data_preprocessing.images_preprocessing import (preprocessing_X, preprocessing_y)
+from facial_keypoints_detection.data_preprocessing.images_preprocessing import make_pipeline
 from keras.optimizers import Adam
 import tensorflow as tf
 
 
+batch_size = 32
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
 if gpu_devices:
    tf.config.experimental.set_memory_growth(gpu_devices[0], True)
 
-train_path = '../data_for_learning/training.csv'
-train = pd.read_csv(train_path)
-X_train = preprocessing_X(train, 96)
-train.drop('Image', axis=1, inplace=True)
-y_train = preprocessing_y(train)
-
 
 model = keras.Sequential([
-    keras.layers.Input((1, 96, 96), batch_size=16, dtype='float32'),
-    keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    keras.layers.Input((1, 96, 96), batch_size=32, dtype='float32'),
+    keras.layers.Conv2D(32, (3, 3), activation='relu', data_format='channels_first'),
     keras.layers.MaxPooling2D((2, 2), strides=1),
     keras.layers.Conv2D(64, (3, 3), strides=1, activation='relu'),
     keras.layers.MaxPooling2D((2, 2), strides=1),
@@ -34,7 +29,11 @@ model = keras.Sequential([
 ])
 
 
+dataset_path = '../data_for_learning/training.csv'
+dataset = pd.read_csv(dataset_path)
+train, val = make_pipeline(dataset, 96, batch_size)
+
 optimizer = Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='mae', metrics=['accuracy'])
-history = model.fit(X_train.reshape(-1, 1, 96, 96), y_train, epochs=100, batch_size=32, validation_split=0.2)
+history = model.fit(train, epochs=100, batch_size=batch_size, validation_data=val)
 model.save('facial_keypoints_detection(CNN, 3 epochs).keras')
--- facial_keypoints_detection/data_preprocessing/images_preprocessing.py
+++ facial_keypoints_detection/data_preprocessing/images_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 
 def convertor(img: np.array, img_size) -> np.array:
@@ -9,18 +10,16 @@ def convertor(img: np.array, img_size) -> np.array:
     return img
 
 
-def preprocessing_X(X: pd.DataFrame, img_size: int,) -> np.array:
+def preprocessing_X(X: pd.DataFrame, img_size: int) -> np.array:
     X.ffill(inplace=True)
-    m, n = X.shape
+    m = X.shape[0]
     img_lst = []
     for i in range(m):
         img = np.array(X['Image'][i].split(), dtype='float32')
         img = convertor(img, img_size)
         img_lst.append(img)
     img_array = np.array(img_lst, dtype='float32')
-    print(img_array.shape)
-    X = tf.data.Dataset.from_tensor_slices(img_array)
-    return X
+    return img_array
 
 
 def preprocessing_y(y: pd.DataFrame) -> np.array:
@@ -29,5 +28,18 @@ def preprocessing_y(y: pd.DataFrame) -> np.array:
         preprocessed_y = y.iloc[i, :].values
         keypoints_lst.append(preprocessed_y)
     keypoints_array = np.array(keypoints_lst, dtype='float32')
-    y = tf.data.Dataset.from_tensor_slices(keypoints_array)
-    return y
+    return keypoints_array
+
+
+def make_pipeline(dataset: pd.DataFrame, img_size: int, batch_size: int) -> tuple[tf.data.Dataset, tf.data.Dataset]:
+    X_dataset = preprocessing_X(dataset, img_size)
+    dataset.drop('Image', axis=1, inplace=True)
+    y_dataset = preprocessing_y(dataset)
+    X_train, X_val, y_train, y_val = train_test_split(X_dataset, y_dataset, test_size=0.3)
+    X_train = tf.data.Dataset.from_tensor_slices(X_train)
+    X_val = tf.data.Dataset.from_tensor_slices(X_val)
+    y_train = tf.data.Dataset.from_tensor_slices(y_train)
+    y_val = tf.data.Dataset.from_tensor_slices(y_val)
+    zipped_train = tf.data.Dataset.zip((X_train, y_train)).batch(batch_size)
+    zipped_test = tf.data.Dataset.zip((X_val, y_val)).batch(batch_size)
+    return zipped_train, zipped_test
