aee6ff1de8533ce2fb79aa7b3c3576dd9875794b
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-04-22 22:25:47 +0500

    Fixed some bugs

--- facial_keypoints_detection/CNNs/facial_keypoints_detection(CNN_keras).py
+++ facial_keypoints_detection/CNNs/facial_keypoints_detection(CNN_keras).py
@@ -3,19 +3,20 @@ from keras import Model
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense, BatchNormalization, Input
 from keras.optimizers import Adam
 from facial_keypoints_detection.data_preprocessing.images_preprocessing import make_pipeline
+from facial_keypoints_detection.draw.draw_result import draw
 
 
 def model_constructor(inputA):
     dr = 0.1
     x = inputA
-    x = Conv2D(32, kernel_size=3, activation='relu', data_format='channels_first', padding='same')(x)
-    x = MaxPooling2D(2, data_format='channels_first')(x)
+    x = Conv2D(32, kernel_size=3, activation='relu', padding='same')(x)
+    x = MaxPooling2D(2)(x)
     x = BatchNormalization()(x)
-    x = Conv2D(64, kernel_size=3, activation='relu', data_format='channels_first', padding='same')(x)
-    x = MaxPooling2D(2, data_format='channels_first')(x)
+    x = Conv2D(64, kernel_size=3, activation='relu', padding='same')(x)
+    x = MaxPooling2D(2)(x)
     x = BatchNormalization()(x)
-    x = Conv2D(128, kernel_size=3, activation='relu', data_format='channels_first', padding='same')(x)
-    x = MaxPooling2D(2, data_format='channels_first')(x)
+    x = Conv2D(128, kernel_size=3, activation='relu', padding='same')(x)
+    x = MaxPooling2D(2)(x)
     x = BatchNormalization()(x)
     x = Flatten()(x)
     x = Dense(512, activation='relu')(x)
@@ -29,7 +30,7 @@ def model_constructor(inputA):
 dataset_path = '../data_for_learning/training.csv'
 dataset = pd.read_csv(dataset_path)
 batch_size = 32
-input_shape = (1, 96, 96)
+input_shape = (96, 96, 1)
 
 train, val = make_pipeline(dataset, 96, batch_size, augmentation=True)
 
@@ -40,6 +41,6 @@ print(model.summary())
 
 optimizer = Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='mse', metrics=['accuracy'])
-history = model.fit(train, validation_data=val, epochs=10, verbose=1)
-
+history = model.fit(train, validation_data=val, epochs=1, verbose=1)
+draw(val, model)
 model.save('facial_keypoints_detection(CNN, 3 epochs).keras')
