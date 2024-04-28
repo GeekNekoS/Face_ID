d4204b6f75bc92d36b6356520ca83bdfc7f13fd7
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-04-28 21:28:16 +0500

    Face alignment with mediapipe

---
+++ face_alignment_with_mediapipe.py
@@ -0,0 +1,13 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+base_options = python.BaseOptions(model_asset_path='facial_keypoints_detection/FaceMesh/face_landmarker.task')
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision import face_aligner
+
+
+def align_face(image_path):
+    options = vision.FaceAlignerOptions(base_options=base_options)
+    FaceAligner = face_aligner.FaceAligner.create_from_options(options)
+    img = mp.Image.create_from_file(image_path)
+    result = FaceAligner.align(img)
+    return result.numpy_view()
