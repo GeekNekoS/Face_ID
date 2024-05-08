882ec874dba52c74c08517c56cb02affe970b4af
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-05-08 13:57:24 +0500

    Added some functions in CNN_keras.py and deleted extra folder

--- facial_key_points_detection/CNNs/CNN_keras.py
+++ facial_key_points_detection/CNNs/CNN_keras.py
@@ -6,6 +6,10 @@ from keras.optimizers import Adam
 from facial_key_points_detection.data_preprocessing.images_preprocessing import make_pipeline
 
 
+def input_base(input_shape, name='input_image'):
+    return Input(input_shape, name=name)
+
+
 def model_constructor(inputA):
     dr = 0.1
     x = inputA
@@ -32,19 +36,5 @@ def model_constructor(inputA):
     return x
 
 
-dataset_path = '../data_for_learning/training.csv'
-dataset = pd.read_csv(dataset_path)
-batch_size = 32
-input_shape = (96, 96, 1)
-
-train, val = make_pipeline(dataset, 96, 96, batch_size)
-
-inputA = Input(shape=input_shape, name='input_image')
-final = model_constructor(inputA)
-model = Model(inputs=[inputA], outputs=final)
-print(model.summary())
-
-optimizer = Adam(learning_rate=0.001)
-model.compile(optimizer=optimizer, loss=keras.losses.mean_absolute_error, metrics=[keras.losses.huber, keras.losses.log_cosh])
-my_callbacks = [keras.callbacks.ModelCheckpoint(filepath='modelCNN(mae).h5', monitor='val_loss', verbose=1, mode='min', save_best_only=True)]
-history = model.fit(train, validation_data=val, epochs=15, verbose=1, callbacks=my_callbacks)
+def make_model(input_layers):
+    return Model(inputs=[input_layers], outputs=model_constructor(input_layers))
