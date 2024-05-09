4a062323197141acc810dd26edb159821373b43b
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-05-10 02:06:19 +0500

    Fixed bug.

--- face_compare/test.py
+++ face_compare/test.py
@@ -1,13 +1,14 @@
 from face_recognition.detect_face import detect_face
 from face_compare.compare2faces import Img2VecModel, cos_similarity
+import cv2
 model = Img2VecModel()
 
 
 def test(image_path1, image_path2):
     coordinates1 = detect_face(image_path1, image_path1)
     coordinates2 = detect_face(image_path2, image_path2)
-    face1 = image_path1[coordinates1[0][1]:coordinates1[0][1]+coordinates1[0][3]+1, coordinates1[0][0]:coordinates1[0][0]+coordinates1[0][2]+1]
-    face2 = image_path1[coordinates2[0][1]:coordinates2[0][1]+coordinates2[0][3]+1, coordinates2[0][0]:coordinates2[0][0]+coordinates2[0][2]+1]
+    face1 = cv2.imread(image_path1)[coordinates1[0][1]:coordinates1[0][1]+coordinates1[0][3]+1, coordinates1[0][0]:coordinates1[0][0]+coordinates1[0][2]+1, :]
+    face2 = cv2.imread(image_path2)[coordinates2[0][1]:coordinates2[0][1]+coordinates2[0][3]+1, coordinates2[0][0]:coordinates2[0][0]+coordinates2[0][2]+1, :]
     return cos_similarity(model.get_vec(face1), model.get_vec(face2))
 
 
