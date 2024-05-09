72cd95abcd1a53bea8c69dbba9b623310066ee89
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-05-09 22:54:25 +0500

    Added files for tests

--- facial_key_points_detection/utils/single_image_preprocessing.py
+++ facial_key_points_detection/utils/single_image_preprocessing.py
@@ -6,4 +6,5 @@ from PIL import Image
 
 def convert_image(image: Image, new_size=(96, 96)) -> np.ndarray:
     converted_image = preprocess_image(image, new_size, cv2.COLOR_BGR2GRAY)
+    np.expand_dims(converted_image, axis=-1)
     return converted_image
--- image/single_image_preprocessing.py
+++ image/single_image_preprocessing.py
@@ -1,11 +1,21 @@
-from image.resize import resize_image
 import cv2
 import numpy as np
+
+
+def resize_image(image, new_size: tuple[int, int]):
+    """
+    Resize image.
+    :param image: PIL Image, image
+    :param new_size: tuple, new image size
+    :return: PIL Image, image with new size
+    """
+    image = cv2.resize(image, new_size)
+    return image
 
 
 def preprocess_image(image, new_size, convert_color_options=None):
     resized_image = resize_image(image, new_size)
     if convert_color_options is not None:
-        resized_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        resized_image = cv2.cvtColor(resized_image, convert_color_options)
     resized_image = np.expand_dims(resized_image, axis=0)
     return resized_image
