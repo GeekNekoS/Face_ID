3ae7e99f15b9144d50a0e79990b29f94dd07f4d7
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-05-17 16:10:23 +0500

    Reworked some functions for facial key points detection and face segmentation

--- face_segmentation/utils/single_image_preprocessing.py
+++ face_segmentation/utils/single_image_preprocessing.py
@@ -1,5 +1,6 @@
 from image.single_image_preprocessing import preprocess_image
 import cv2
+import numpy as np
 
 
 def convert_image(image, new_size=(512, 512)):
@@ -10,4 +11,6 @@ def convert_image(image, new_size=(512, 512)):
     :return: ndarray, converted image
     """
     converted_image = preprocess_image(image, new_size, cv2.COLOR_BGR2RGB)
+    converted_image /= 255
+    converted_image = np.expand_dims(converted_image, axis=0)
     return converted_image
--- facial_key_points_detection/utils/settings.py
+++ facial_key_points_detection/utils/settings.py
@@ -1,4 +1,4 @@
 import keras
 
 
-FACIAL_KEY_POINTS_MODEL_CUSTOM_OBJECTS = {'mae': keras.losses.mean_absolute_error, 'huber': keras.losses.huber, 'log_cosh': keras.losses.log_cosh}
+FACIAL_KEY_POINTS_MODEL_CUSTOM_OBJECTS = {'mae': keras.losses.mean_absolute_error}
