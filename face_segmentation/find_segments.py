5e1781a303ee457e8f8d13b889a1af04ee3142f4
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-05-24 18:45:10 +0500

    Added the ability to use a variable with a model

--- face_segmentation/find_segments.py
+++ face_segmentation/find_segments.py
@@ -4,16 +4,19 @@ from face_segmentation.load_U_net import load_model
 from face_segmentation.utils.settings import U_NET_CUSTOM_OBJECTS
 
 
-def find_segments(image: np.ndarray, model_path=None) -> np.ndarray:
+def find_segments(image: np.ndarray, model_path=None, model=None, custom_objects: dict = U_NET_CUSTOM_OBJECTS) -> np.ndarray:
     """
     Find 11 face segments (background, skin, left eyebrow, right eyebrow, left eye, right eye, nose, upper lip, inner mouth, lower lip, hair).
     :param image: ndarray, array from image
     :param model_path: str, U-net model path [optional], if None, then the model will be downloaded from Yandex disk
+    :param model: model, model for face segmentation [optional], if None, then the model will be downloaded from Yandex disk
+    :param custom_objects: dict, custom objects for load models [optional], initially contains the necessary objects for the neural network from Yandex disk
     :return: ndarray, array from segments
     """
-    if model_path is None:
-        model = import_model(U_NET_CUSTOM_OBJECTS)
+    if model_path is None and model is None:
+        model = import_model(custom_objects)
     else:
-        model = load_model(model_path, U_NET_CUSTOM_OBJECTS)
+        if model_path is not None:
+            model = load_model(model_path, custom_objects)
     results = model.predict(image)
     return results
--- facial_key_points_detection/find_facial_key_points.py
+++ facial_key_points_detection/find_facial_key_points.py
@@ -4,23 +4,19 @@ from facial_key_points_detection.load_facial_key_points_model import load_model
 from facial_key_points_detection.utils.settings import FACIAL_KEY_POINTS_MODEL_CUSTOM_OBJECTS
 
 
-def find_facial_key_points(image: np.ndarray, model_path: str = None, custom_objects: dict = None) -> np.ndarray:
+def find_facial_key_points(image: np.ndarray, model_path: str = None, model=None, custom_objects: dict = FACIAL_KEY_POINTS_MODEL_CUSTOM_OBJECTS) -> np.ndarray:
     """
     Returns normalized facial key points coordinates.
     :param image: ndarray, three-dimensional array from image (image is black and white)
     :param model_path: str, facial key points model path [optional], if None, then the model will be downloaded from Yandex disk
-    :param custom_objects: dict, custom objects for load models [optional]
+    :param model: model, model for face segmentation [optional], if None, then the model will be downloaded from Yandex disk
+    :param custom_objects: dict, custom objects for load models [optional], initially contains the necessary objects for the neural network from Yandex disk
     :return: ndarray, two-dimensional array of predicted facial key points
     """
-    if model_path is None:
-        if custom_objects is None:
-            model = import_model(FACIAL_KEY_POINTS_MODEL_CUSTOM_OBJECTS)
-        else:
-            model = import_model(custom_objects)
+    if model_path is None and model is None:
+        model = import_model(custom_objects)
     else:
-        if custom_objects is None:
-            model = load_model(model_path, FACIAL_KEY_POINTS_MODEL_CUSTOM_OBJECTS)
-        else:
+        if model_path is not None:
             model = load_model(model_path, custom_objects)
     result = np.array(model.predict(image)[0])
     return result
